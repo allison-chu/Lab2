b6ff04e5938d78f30cf9b3825ca3f2de1e99cb4f
Dylan <drossi517@gmail.com>
2021-01-25 15:58:15 -0800

    Add files via upload

---
+++ hello2Function.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Dylan Rossi on 1/24/21.
+//
+
+#include <iostream>
+using namespace std;
+int hello2Function(string name2) {
+    cout << "hello World " << name2 << endl;
+    return 0; }
